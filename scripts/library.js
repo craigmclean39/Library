b6671079c7bbcb342636e88b635709f2778a1890
Craig McLean <craig.mclean39@gmail.com>
2021-07-17 16:57:17 -0700

    Generating a basic card in JS from a Book Object.

--- scripts/library.js
+++ scripts/library.js
@@ -1,5 +1,8 @@
 let myLibrary = [];
 
+const libraryFlex = document.getElementById("library-flex");
+console.log(libraryFlex);
+
 let Book = function(title, author, pages, read, desc, category, thumbnail) {
 
     //console.log("in book constructor")
@@ -32,7 +35,102 @@ Book.prototype.info = function(){
     return `${this.title} by ${this.author}, ${this.pages} pages, ${this.readOrNotStr()}, ${this.desc}, ${this.category}, ${this.thumbnail}`;
 }
 
+//This function takes a Book object and generates a html card to be added to the dom
+{/* <div class='card'>
+        <div class='hero'></div>
+        <div class="bottom">
+         <div class ='book-title book-info'>
+           The Martian
+         </div>
+     
+         <div class="author book-info">Andy Weir</div>
+         
+         <div class="desc">
+     After a dust storm nearly kills him and forces his crew to evacuate while thinking him dead, Mark finds himself stranded and completely alone with no way to even signal Earth that he’s alive</div>
+         
+         <div class='category book-info'>
+           <div class="info-left">
+             <div class='fic'>Fiction</div>
+             <div class="genre">Sci-fi</div>
+           </div>
+           <div class="pages">400 pages</div>
+         </div>
+         <div class="card-footer">
+           <div>
+             <button class="read btn" value="read">
+                <img src="media/book_black_24dp.svg">
+                <div>Read</div>
+             </button>
+             </div>
+           <div>
+             <button class="delete btn" value = "save">
+               <img src="media/delete_outline_black_24dp.svg">
+               Delete
+             </button>
+           </div>
+         </div>
+        </div>
+       </div> */}
+function GenerateCard(b){
+
+    let newCard = document.createElement("div");
+    newCard.className = 'card';
+
+    let hero = document.createElement("div");
+    hero.className = 'hero';
+    hero.style.backgroundImage = `url("${b.thumbnail}")`;
+    newCard.appendChild(hero);
+    
+
+    let bottom = document.createElement("div");
+    bottom.className = "bottom";
+
+
+    let title = document.createElement("div");
+    title.className = "book-title book-info";
+    title.innerText = b.title;
+
+    let author = document.createElement("div");
+    author.className = "author book-info";
+    author.innerText = b.author;
+
+    let description = document.createElement("div");
+    description.className = "desc";
+    description.innerText = b.desc;
+
+    bottom.appendChild(title);
+    bottom.appendChild(author);
+    bottom.appendChild(description);
+
+    let category = document.createElement("div");
+    category.className = "category book-info";
+
+    let infoLeft = document.createElement("div");
+    infoLeft.className = "info-left";
+
+    let genre = document.createElement("div");
+    genre.className = "genre";
+    genre.innerText = b.category;
+
+    infoLeft.appendChild(genre);
+
+    let pages = document.createElement("div");
+    pages.className = "pages";
+    pages.innerText = b.pages + " pages";
+    
+    category.appendChild(infoLeft);
+    category.appendChild(pages);
+
+    bottom.appendChild(category);
+
+    newCard.appendChild(bottom);
+
+    return newCard;
+}
+
 
 //Some hardcoded books to default into the library
-let theMartian = Book("The Martian", "Andy Weir", 480, false, "Description", "Fiction", "media/9780804139021.jpeg");
+let theMartian = new Book("The Martian", "Andy Weir", 480, false, "Description", "Fiction", "media/9780804139021.jpeg");
 console.log(theMartian.info());
+
+libraryFlex.appendChild(GenerateCard(theMartian));

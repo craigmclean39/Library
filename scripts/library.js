2e984890a172fb95bcf9369e88606dfdd9235b70
Craig McLean <craig.mclean39@gmail.com>
2021-07-18 11:31:25 -0700

    Working on adding a book form when the user clicks to add a book. Then when they hit save it adds a proper book to the library.

--- scripts/library.js
+++ scripts/library.js
@@ -1,6 +1,9 @@
 let myLibrary = [];
 
 const libraryFlex = document.getElementById("library-flex");
+const header = document.getElementById("header");
+
+let addInProgress = false;
 
 let Book = (function(){
     let nextId = 0;
@@ -39,9 +42,28 @@ Book.prototype.info = function(){
     return `${this.title} by ${this.author}, ${this.pages} pages, ${this.readOrNotStr()}, ${this.desc}, ${this.category}, ${this.thumbnail}, ${this.id}`;
 }
 
+function SaveBook(evt)
+{
+    console.dir(evt);
+
+    let elmt = evt.target;
+    while(elmt.className != "card")
+    {
+        elmt = elmt.parentElement;
+        console.log(elmt.className);
+    }
+
+    libraryFlex.removeChild(elmt);
+
+    let b = new Book("TEST", "", 0, false, "", "", "");
+    AddToLibrary(b);
+
+    addInProgress = false;
+}
+
 function AddToLibrary(b){
     myLibrary.push(b);
-    libraryFlex.appendChild(GenerateCard(b));
+    libraryFlex.appendChild(GenerateCard(b, false));
 }
 
 function DeleteFromLibraryEvent(evt) {
@@ -72,8 +94,77 @@ function DeleteFromLibrary(id) {
             libraryFlex.removeChild(children[i]);
         }
     }
+}
+
+function ToggleReadStatusEvent(evt) {
+    ToogleReadStatus(evt.currentTarget.dataset.id);
+}
+
+function ToogleReadStatus(id) {
+    //Toggle status in Library
+    for(let i = 0; i < myLibrary.length; i++)
+    {
+        if(myLibrary[i].id == id)
+        {
+            myLibrary[i].read = !myLibrary[i].read;
+            break;
+        }
+    }
+
+    //Toggle button in DOM
+    let children = libraryFlex.children;
+    for(let i = 0; i < children.length; i++)
+    {
+        if(children[i].dataset.id == id)
+        {
+            let btns = children[i].getElementsByClassName("btn");
+
+            console.dir(btns);
+
+            for(let j = 0; j < btns.length; j++)
+            {
+                if(btns[j].classList.contains("read"))
+                {
+                    btns[j].classList.remove("read");
+                    btns[j].classList.add("unread");
+                    btns[j].getElementsByTagName("div")[0].innerText = "Unread";
+                    break;
+                }
+                else if(btns[j].classList.contains("unread"))
+                {
+                    btns[j].classList.remove("unread");
+                    btns[j].classList.add("read");
+
+                    btns[j].getElementsByTagName("div")[0].innerText = "Read";
+                    break;
+                }
+            }
+        }
+    }
+
+    console.dir(myLibrary);
+}
+
+function AddBookInput(evt)
+{
+    console.log("ADD");
+
+    if(!addInProgress)
+    {
+        addInProgress = true;
+        let b = new Book("", "", 0, false, "", "", "");
+        libraryFlex.appendChild(GenerateCard(b, true));
+
+        libraryFlex.lastChild.scrollIntoView();
+    }
+}
+
+function DeleteText(evt)
+{
+    evt.currentTarget.value = "";
 
 }
+
 
 function CreateDiv(className) {
 
@@ -82,8 +173,15 @@ function CreateDiv(className) {
     return d;
 }
 
+function CreateInput(className){
+    let d = document.createElement("input");
+    d.type = "text";
+    d.className = className;
+    return d;
+}
+
 //This function takes a Book object and generates a html card to be added to the dom
-function GenerateCard(b){
+function GenerateCard(b, isForm){
 
     let newCard = CreateDiv("card");
     newCard.dataset.id = b.id;
@@ -95,8 +193,24 @@ function GenerateCard(b){
 
     let bottom = CreateDiv("bottom");
 
-    let title = CreateDiv("book-title book-info");
-    title.innerText = b.title;
+    let title = {};
+    if(!isForm)
+    {
+        title = CreateDiv("book-title book-info");
+        title.innerText = b.title;
+
+    }
+    else
+    {
+        title = CreateInput("book-title-input book-title book-info");
+        title.value = "Title..."
+        title.addEventListener("click", DeleteText);
+    }
+    
+
+
+    
+    
 
     let author = CreateDiv("author book-info");
     author.innerText = b.author;
@@ -126,8 +240,26 @@ function GenerateCard(b){
 
     let footer = CreateDiv("card-footer");
 
-    footer.appendChild(AddButton("READ", b.id));
-    footer.appendChild(AddButton("DELETE", b.id));
+
+    if(b.read)
+    {
+        footer.appendChild(AddButton("READ", b.id));
+
+    }
+    else 
+    {
+        footer.appendChild(AddButton("UNREAD", b.id));
+    }
+
+    if(!isForm)
+    {
+        footer.appendChild(AddButton("DELETE", b.id));
+    }
+    else
+    {
+        footer.appendChild(AddButton("SAVE", b.id));
+    }
+    
 
 
     bottom.appendChild(footer);
@@ -148,12 +280,22 @@ function AddButton(buttonType, newId)
 
     switch(buttonType)
     {
+        case "UNREAD":
+            {
+                btn.className = "unread btn";
+                btn.value  = "unread";
+                img.src = "media/book_black_24dp.svg";
+                btnText.innerText = "Unread";
+                btn.addEventListener("click", ToggleReadStatusEvent);
+                break;
+            }
         case "READ":
             {
                 btn.className = "read btn";
                 btn.value  = "read";
                 img.src = "media/book_black_24dp.svg";
                 btnText.innerText = "Read";
+                btn.addEventListener("click", ToggleReadStatusEvent);
                 break;
             }
         case "DELETE":
@@ -172,6 +314,16 @@ function AddButton(buttonType, newId)
                 btn.value  = "save";
                 img.src = "media/save_black_24dp.svg";
                 btnText.innerText = "Save";
+                btn.addEventListener("click", SaveBook);
+                break;
+            }
+            case "ADD":
+            {
+                btn.className = "add btn";
+                btn.value  = "add";
+                img.src = "media/library_add_black_24dp.svg";
+                btnText.innerText = "Add";
+                btn.addEventListener("click", AddBookInput);
                 break;
             }
 }
@@ -186,16 +338,20 @@ function AddButton(buttonType, newId)
 
 }
 
+header.appendChild(AddButton("ADD"));
+
 
 //Some hardcoded books to default into the library
-let theMartian = new Book("The Martian", "Andy Weir", 480, false, "Description", "Fiction", "media/9780804139021.jpeg");
+let theMartian = new Book("The Martian", "Andy Weir", 480, true, "Description", "Fiction", "media/9780804139021.jpeg");
 let anathem = new Book("Anathem", "Neal Stephenson", 1008, false, "Description", "Fiction", "media/anathem.jpg");
+
 
 //console.log(theMartian.info());
 //console.log(anathem.info());
 
 AddToLibrary(theMartian);
 AddToLibrary(anathem);
+
 
 console.dir(myLibrary);
 
